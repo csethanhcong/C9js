43f0451dcc33bcef655282c15020c0c651243086
congpt <congpt.shinway@gmail.com>
2016-03-01 11:33:36 +0700

    update

--- public/js/controllers/DemoCtrl.js
+++ public/js/controllers/DemoCtrl.js
@@ -2,64 +2,43 @@ angular.module('demoApp')
   .controller('DemoCtrl', function($scope, $compile, $element, fileReader){
     $scope.rows = [];
     $scope.title = "DemoCtrl";
-    $scope.d3Data = [
-      {name: "Greg",  data: 98},
-      {name: "Ari",   data: 96},
-      {name: "Loser", data: 48}
-    ];
-    $scope.accessor = function(d){ return d.data };
-    $scope.d3OnClick = function(item){
+    $scope.d3Data = [{
+      name: "Greg",
+      data: 98
+    }, {
+      name: "Ari",
+      data: 96
+    }, {
+      name: "Loser",
+      data: 48
+    }];
+
+    $scope.accessor = function(d) {
+      return d.data
+    };
+
+    $scope.d3OnClick = function(item) {
       alert(item.name);
     };
 
-    // $scope.addData = function() {
-    //   var html = '<div class="row">'+
-    //   '  <div class="row">' +
-    //   '    <div class="col-lg-4">' +
-    //   '      <input type="text" class="form-control" placeholder="name.."></input>' +
-    //   '    </div>' +
-    //   '    <div class="col-lg-4">' +
-    //   '      <input type="text" class="form-control" placeholder="data.."></input>' +
-    //   '    </div>' +
-    //   '    <div class="col-lg-4">' +
-    //   '      <button class="btn btn-primary" type="button" title="Add" ng-click="addData()" ><span><i class="glyphicon glyphicon-plus"></i></span></button>' +
-    //   '      <button class="btn btn-danger" type="button" title="Remove" ng-click="removeData($event)"><span><i class="glyphicon glyphicon-remove"></i></span></button>' +
-    //   '    </div>' +
-    //   '  </div>' +
-    //   '  <div class="col-lg-10"><hr></div>' +
-    //   '</div>';
-    //   html = $compile(html)($scope);
-    //   var appendToElement = angular.element($('#input-data'));
-    //   appendToElement.append(html);
-    // };
+    $scope.getFile = function() {
+      fileReader.readAsDataUrl($scope.file, $scope)
+        .then(function(result) {
+          var resultInJSON = JSON.parse(result);
 
-    // $scope.removeData = function(e) {
-    //   console.log(e);
-    //   // Choose exact row that contains this element
-    //   var removeElement = e.path[3].className == 'row ng-scope' ? e.path[3] : e.path[5];
-    //   removeElement.remove();
-    // };
+          // refresh data
+          $scope.refreshData();
 
-    $scope.getFile = function () {
-        fileReader.readAsDataUrl($scope.file, $scope)
-          .then(function(result) {
-              // $scope.$apply(function() {
-                var resultInJSON = JSON.parse(result);
-                // refresh data
-                // $scope.refreshData();
-                for (var i = 0; i < resultInJSON.length; i++){
-                  $scope.d3Data.push({
-                    name: resultInJSON[i].name,
-                    data: resultInJSON[i].data
-                  });
-                };     
-              });
-          // });
+          for (var i = 0; i < resultInJSON.length; i++) {
+            $scope.d3Data.push({
+              name: resultInJSON[i].name,
+              data: resultInJSON[i].data
+            });
+          };
+        });
     };
 
-    // $scope.refreshData = function() {
-    //   // $scope.$apply(function() {
-    //     $scope.d3Data = [];
-    //   // });
-    // };
+    $scope.refreshData = function() {
+      $scope.d3Data = [];
+    };
   });
---
+++ public/js/directives/ngFileSelect.js
@@ -0,0 +1,11 @@
+angular.module('demoApp')
+	.directive("ngFileSelect", function() {
+		return {
+			link: function($scope, el) {
+				el.bind("change", function(e) {
+					$scope.file = (e.srcElement || e.target).files[0];
+					$scope.getFile();
+				})
+			}
+		}
+	});
